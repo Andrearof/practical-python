06563ab16e1d70e61cfb1502f8213853cd8d83b9
Andrearof <andrea.rofini@student.univaq.it>
2023-07-30 11:14:48 +0200

    Exercise 1.8

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,19 +1,31 @@
 # mortgage.py
 #
-# Exercise 1.7
+# Exercise 1.8
 """
 Dave has decided to take out a 30-year fixed rate mortgage of $500,000 with Guido’s Mortgage, Stock Investment, and Bitcoin trading corporation.
- The interest rate is 5% and the monthly payment is $2684.11.
+The interest rate is 5% and the monthly payment is $2684.11.
 Here is a program that calculates the total amount that Dave will have to pay over the life of the mortgage:
+
+Suppose Dave pays an extra $1000/month for the first 12 months of the mortgage.
+Modify the program to incorporate this extra payment and have it print the total amount paid along with the number of months required.
 """
 
 principal = 500000.0
 rate = 0.05
 payment = 2684.11
 total_paid = 0.0
+months = 0
+extra = 1000
+monthly_payment = 0
 
 while principal > 0:
-    principal = principal * (1 + rate / 12) - payment
-    total_paid = total_paid + payment
+    if months < 12:
+        monthly_payment = payment + extra
+    else:
+        monthly_payment = payment
+    principal = principal * (1 + rate / 12) - monthly_payment
+    total_paid = total_paid + monthly_payment
+    months += 1
 
-print("Total paid", total_paid)
+print("Total paid: ", total_paid)
+print("Number of months required: ", months)
