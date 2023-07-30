122c324fac2716a9b28825fdcf6873f95fd3dcac
Andrearof <andrea.rofini@student.univaq.it>
2023-07-30 11:22:40 +0200

    Exercise 1.9

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,13 +1,19 @@
 # mortgage.py
 #
-# Exercise 1.8
+# Exercise 1.9
 """
 Dave has decided to take out a 30-year fixed rate mortgage of $500,000 with Guido’s Mortgage, Stock Investment, and Bitcoin trading corporation.
 The interest rate is 5% and the monthly payment is $2684.11.
 Here is a program that calculates the total amount that Dave will have to pay over the life of the mortgage:
 
+1.8:
 Suppose Dave pays an extra $1000/month for the first 12 months of the mortgage.
 Modify the program to incorporate this extra payment and have it print the total amount paid along with the number of months required.
+
+1.9:
+Modify the program so that extra payment information can be more generally handled
+Make the program look at these variables and calculate the total paid appropriately.
+How much will Dave pay if he pays an extra $1000/month for 4 years starting after the first five years have already been paid
 """
 
 principal = 500000.0
@@ -15,12 +21,18 @@ rate = 0.05
 payment = 2684.11
 total_paid = 0.0
 months = 0
-extra = 1000
+extra_payment_start_month = int(
+    input("Enter number of the month of the first extra payment: ")
+)
+extra_payment_end_month = int(
+    input("Enter number of the month of the last extra payment: ")
+)
+extra_payment = int(input("Enter the value of the extra payment: "))
 monthly_payment = 0
 
 while principal > 0:
-    if months < 12:
-        monthly_payment = payment + extra
+    if extra_payment_start_month < months < extra_payment_end_month:
+        monthly_payment = payment + extra_payment
     else:
         monthly_payment = payment
     principal = principal * (1 + rate / 12) - monthly_payment
