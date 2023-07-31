2b3c6aec315f8a89c3a6e9ac72ac193726c63cd5
Andrearof <andrea.rofini@student.univaq.it>
2023-07-31 21:48:55 +0200

    Exercise 1.22

--- Work/lists.py
+++ Work/lists.py
@@ -7,11 +7,54 @@ The for loop works by looping over data in a sequence such as a list.
 """
 
 for s in symlist:
-    print("s=", s)
+    pass
+    # print("s=", s)
 
 """
 Use the in or not in operator to check if 'AIG','AA', and 'CAT' are in the list of symbols
 """
-print("AIG" in symlist)  # True
-print("AA" in symlist)  # False
-print("CAT" not in symlist)  # True
+# print("AIG" in symlist)  # True
+# print("AA" in symlist)  # False
+# print("CAT" not in symlist)  # True
+
+# Exercise 1.22
+
+"""
+Use the append() method to add the symbol 'RHT' to end of symlist
+"""
+symlist.append("RHT")
+# print(symlist) # ['HPQ', 'AAPL', 'AIG', 'MSFT', 'YHOO', 'GOOG', 'RHT']
+
+"""
+Use the insert() method to insert the symbol 'AA' as the second item in the list.
+"""
+symlist.insert(1, "AA")
+# print(symlist) # ['HPQ', 'AA', 'AAPL', 'AIG', 'MSFT', 'YHOO', 'GOOG', 'RHT']
+
+"""
+Use the remove() method to remove 'MSFT' from the list.
+"""
+symlist.remove("MSFT")
+# print(symlist)  # ['HPQ', 'AA', 'AAPL', 'AIG', 'YHOO', 'GOOG', 'RHT']
+
+"""
+Append a duplicate entry for 'YHOO' at the end of the list
+"""
+symlist.append("YHOO")
+# print(symlist)  # ['HPQ', 'AA', 'AAPL', 'AIG', 'YHOO', 'GOOG', 'RHT', 'YHOO']
+
+"""
+Use the index() method to find the first position of 'YHOO' in the list.
+"""
+# print(symlist.index("YHOO"))  # 4
+
+"""
+Count how many times 'YHOO' is in the list:
+"""
+# print(symlist.count("YHOO"))  # 2
+
+"""
+Remove the first occurrence of 'YHOO'.
+"""
+symlist.remove("YHOO")
+# print(symlist)  # ['HPQ', 'AA', 'AAPL', 'AIG', 'GOOG', 'RHT', 'YHOO']
