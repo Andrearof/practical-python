286a2a4b669ad23fb04ac1aa97e10b4ce196dd8b
Andrearof <andrea.rofini@student.univaq.it>
2023-07-31 22:43:15 +0200

    Exercise 1.31

--- Work/functions.py
+++ Work/functions.py
@@ -10,5 +10,26 @@ def greeting(name):
     print(f"Hello {name}")
 
 
-greeting("Guido")
-greeting("Paula")
+# greeting("Guido")
+# greeting("Paula")
+
+# Exercise 1.30
+
+"""
+Turn pcost.py into a function that takes a filename and returns the total cost
+"""
+
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, "rt") as file:
+        next(file)
+        for line in file:
+            row = line.split(",")
+            total_cost += float(row[1]) * float(row[2])
+
+    return total_cost
+
+
+cost = portfolio_cost("Work/Data/portfolio.csv")
+print(f"Total cost: {cost}")
