59ae5bb7b7bf2808c1fa2e3fc006fb87957b7c90
Andrearof <andrea.rofini@student.univaq.it>
2023-07-31 20:35:14 +0200

    Exercise 1.14

---
+++ Work/strings.py
@@ -0,0 +1,12 @@
+# Exercise 1.14
+
+"""
+Concatenates a new symbol “GOOG” to the end of symbols
+so that the symbols variable holds the value 'AAPL,IBM,MSFT,YHOO,SCO,GOOG'
+"""
+
+symbols = "AAPL,IBM,MSFT,YHOO,SCO"
+
+symbols = symbols + ",GOOG"
+
+print(symbols)
