bad35ad0b5b30bdac2624d042c46a720ff8bf015
andrearof <andrea.rofini@student.univaq.it>
2023-08-01 20:36:56 +0200

    Exercise 2.2: Dictionaries as a data structure

--- Work/data_structures.py
+++ Work/data_structures.py
@@ -14,4 +14,13 @@ row = next(rows)
 t = row[0], int(row[1]), float(row[2])
 name, share, price = t
 total_cost = share * price
-print(f"Total cost: {total_cost}")
+# print(f"Total cost: {total_cost}")
+
+# Exercise 2.2: Dictionaries as a data structure
+
+"""
+Create a dictionary instead and calculate the cost.
+"""
+
+d = {"name": row[0], "shares": int(row[1]), "price": float(row[2])}
+print(f"Total cost: {d['shares'] * d['price']}")
