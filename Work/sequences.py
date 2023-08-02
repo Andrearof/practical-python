14f9f8e9d1b4d571302d6053aaf7c39c48dbf00b
Andrearof <andrea.rofini@student.univaq.it>
2023-08-02 18:07:28 +0200

    Exercise 2.13: Counting

---
+++ Work/sequences.py
@@ -0,0 +1,15 @@
+# Exercise 2.13: Counting
+
+"""
+Basic counting examples
+"""
+
+for n in range(10):
+    # print(n, end=" ")  # 0 ... 9
+    continue
+for n in range(10, 0, -1):
+    # print(n, end=" ")  # 10 ... 1
+    continue
+for n in range(0, 10, 2):
+    print(n, end=" ")  # 0, 2, ..., 8
+    continue
