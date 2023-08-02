99e2b422775b873d57a6c1444b86a58c17a1cd89
Andrearof <andrea.rofini@student.univaq.it>
2023-08-02 18:27:17 +0200

    Exercise 2.15: A practical enumerate() example

--- Work/functions.py
+++ Work/functions.py
@@ -12,34 +12,3 @@ def greeting(name):
 
 # greeting("Guido")
 # greeting("Paula")
-
-# Exercise 1.30
-
-"""
-Turn pcost.py into a function that takes a filename and returns the total cost
-"""
-
-
-def portfolio_cost(filename):
-    total_cost = 0
-    with open(filename, "rt") as file:
-        next(file)
-        for line in file:
-            row = line.split(",")
-            try:
-                total_cost += float(row[1]) * float(row[2])
-            except ValueError:
-                print("Couldn't parse data, invalid format.")
-
-    return total_cost
-
-
-cost = portfolio_cost("Work/Data/portfolio.csv")
-# print(f"Total cost: {cost}")
-
-# Exercise 1.31
-
-"""
-Catch the exception
-"""
-portfolio_cost("Work/Data/missing.csv")
--- Work/pcost.py
+++ Work/pcost.py
@@ -9,11 +9,42 @@ Write a program called pcost.py that opens this file, reads all lines, and calcu
 Output = Total cost 44671.15
 """
 
-total_cost = 0
-with open("Work/Data/portfolio.csv", "rt") as file:
-    next(file)
-    for line in file:
-        row = line.split(",")
-        total_cost += float(row[1]) * float(row[2])
+# Exercise 1.30
 
-print(f"Total cost: {total_cost}")
+"""
+Turn pcost.py into a function that takes a filename and returns the total cost
+"""
+
+# Exercise 1.31
+
+"""
+Catch the exception
+"""
+
+# Exercise 2.15: A practical enumerate() example
+
+"""
+Recall that the file Data/missing.csv contains data for a stock portfolio, but has some rows with missing data. 
+Using enumerate(), modify your pcost.py program so that it prints a line number with the warning message when it encounters bad input
+"""
+
+
+def portfolio_cost(filename):
+    total_cost = 0
+    with open(filename, "rt") as file:
+        next(file)
+        for number, line in enumerate(file, start=1):
+            row = line.split(",")
+            try:
+                total_cost += float(row[1]) * float(row[2])
+            except ValueError:
+                print(f"Row {number}: Bad row: {row}")
+
+    return total_cost
+
+
+# cost = portfolio_cost("Work/Data/portfolio.csv")
+# print(f"Total cost: {cost}")
+
+cost = portfolio_cost("Work/Data/missing.csv")
+print(cost)
