1dd27e39fb9d048ffea335d34acc76a7ecd1db2a
Andrearof <andrea.rofini@student.univaq.it>
2023-08-04 09:48:13 +0200

    Exercise 3.3: Reading CSV Files

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,3 +1,28 @@
 # fileparse.py
 #
 # Exercise 3.3
+
+import csv
+
+
+def parse_csv(filename):
+    """
+    Parse a CSV file into a list of records
+    """
+    with open(filename) as file:
+        rows = csv.reader(file)
+
+        # Headers
+        headers = next(rows)
+        records = []
+        for row in rows:
+            if not row:  # skip with no data
+                continue
+            record = dict(zip(headers, row))
+            records.append(record)
+
+    return records
+
+
+portfolio = parse_csv("Work/Data/portfolio.csv")
+print(portfolio)
