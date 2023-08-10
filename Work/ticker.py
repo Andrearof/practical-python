50bcdb809894e1f1f6d15142fe31e65be29a4704
Andrearof <andrea.rofini@student.univaq.it>
2023-08-10 15:36:57 +0200

    Exercise 6.12: Putting it all together

--- Work/ticker.py
+++ Work/ticker.py
@@ -1,7 +1,9 @@
-# Exercise 6.11: Filtering data
+# Exercise 6.12: Putting it all together
 
 from follow import follow
 import csv
+from report import read_portfolio
+from tableformat import create_formatter
 
 
 def select_columns(rows, indices):
@@ -29,16 +31,22 @@ def parse_stock_data(lines):
     rows = csv.reader(lines)
     rows = select_columns(rows, [0, 1, 4])
     rows = convert_types(rows, [str, float, float])
-    rows = make_dicts(rows, ["name", "price", "change"])
     return rows
 
 
-if __name__ == "__main__":
-    from report import read_portfolio
-
-    portfolio = read_portfolio("Work/Data/portfolio.csv")
-    lines = follow("Work/Data/stocklog.csv")
+def ticker(portfolio_file, log_file, format):
+    portfolio = read_portfolio(portfolio_file)
+    lines = follow(log_file)
     rows = parse_stock_data(lines)
-    rows = filter_symbols(rows, portfolio)
-    for row in rows:
-        print(row)
+    formatter = create_formatter(format)
+
+    formatter.headings(["Name", "Price", "Change"])
+    for name, price, change in rows:
+        rowdata = [name, f"{price:0.2f}", f"{change:0.2f}"]
+        formatter.row(rowdata)
+
+
+if __name__ == "__main__":
+    import sys
+
+    ticker(sys.argv[1], sys.argv[2], sys.argv[3])
