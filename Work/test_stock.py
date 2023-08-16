142e25231844e3bcbf6c368ada124de417fef785
Andrearof <andrea.rofini@student.univaq.it>
2023-08-16 16:21:31 +0200

    Exercise 8.1: Writing Unit Tests

---
+++ Work/test_stock.py
@@ -0,0 +1,31 @@
+# Exercise 8.1: Writing Unit Tests
+
+import unittest
+import stock
+
+
+class TestStock(unittest.TestCase):
+    def test_create(self):
+        s = stock.Stock("GOOG", 100, 490.1)
+        self.assertEqual(s.name, "GOOG")
+        self.assertEqual(s.shares, 100)
+        self.assertEqual(s.price, 490.1)
+
+    def test_cost(self):
+        s = stock.Stock("GOOG", 100, 490.1)
+        cost = s.cost
+        self.assertEqual(cost, 49010.0)
+
+    def test_sell(self):
+        s = stock.Stock("GOOG", 100, 490.1)
+        s.sell(50)
+        self.assertEqual(s.shares, 50)
+
+    def test_bad_shares(self):
+        s = stock.Stock("GOOG", 100, 490.1)
+        with self.assertRaises(TypeError):
+            s.shares = "100"
+
+
+if __name__ == "__main__":
+    unittest.main()
