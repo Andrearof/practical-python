500d7fbe88e8659ead905924c62c48205d38f791
Andrearof <andrea.rofini@student.univaq.it>
2023-08-16 17:36:20 +0200

    Exercise 8.2: Adding logging to a module

--- Work/fileparse.py
+++ Work/fileparse.py
@@ -1,12 +1,26 @@
 # fileparse.py
-import csv
 
-def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', silence_errors=False):
-    '''
+# Exercise 8.2: Adding logging to a module
+
+import csv
+import logging
+
+log = logging.getLogger(__name__)
+
+
+def parse_csv(
+    lines,
+    select=None,
+    types=None,
+    has_headers=True,
+    delimiter=",",
+    silence_errors=False,
+):
+    """
     Parse a CSV file into a list of records with type conversion.
-    '''
+    """
     if select and not has_headers:
-        raise RuntimeError('select requires column headers')
+        raise RuntimeError("select requires column headers")
 
     rows = csv.reader(lines, delimiter=delimiter)
 
@@ -15,17 +29,17 @@ def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', s
 
     # If specific columns have been selected, make indices for filtering and set output columns
     if select:
-        indices = [ headers.index(colname) for colname in select ]
+        indices = [headers.index(colname) for colname in select]
         headers = select
 
     records = []
     for rowno, row in enumerate(rows, 1):
-        if not row:     # Skip rows with no data
+        if not row:  # Skip rows with no data
             continue
 
         # If specific column indices are selected, pick them out
         if select:
-            row = [ row[index] for index in indices]
+            row = [row[index] for index in indices]
 
         # Apply type conversion to the row
         if types:
@@ -33,8 +47,8 @@ def parse_csv(lines, select=None, types=None, has_headers=True, delimiter=',', s
                 row = [func(val) for func, val in zip(types, row)]
             except ValueError as e:
                 if not silence_errors:
-                    print(f"Row {rowno}: Couldn't convert {row}")
-                    print(f"Row {rowno}: Reason {e}")
+                    log.warning("Row %d: Couldn't convert %s", rowno, row)
+                    log.debug("Row %d: Reason %s", rowno, e)
                 continue
 
         # Make a dictionary or a tuple
